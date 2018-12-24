f64c2d565fa30f3c5785a95b84da4f36937ee413
Serg <github-com@idiot.org.ru>
2018-12-24 03:49:01 +0300

    Update PanomNom.js

--- src/PanomNom.js
+++ src/PanomNom.js
@@ -348,9 +348,11 @@
       var level = 0;
       
       do{
-        w /= Math.pow(2,level);
+        if(level){
+          w /= 2 * level;
+          h /= 2 * level;
+        }
         this.widths.unshift(w);
-        h /= Math.pow(2,level);
         this.heights.unshift(h);
         this.tilesW.unshift(Math.ceil(w/this.metadata.tiles.tileSize.width));
         this.tilesH.unshift(Math.ceil(h/this.metadata.tiles.tileSize.height));
